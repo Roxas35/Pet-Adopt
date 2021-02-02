e23714cd4cb7265c74474f7b316accf3b3577a63
Roxas <72147801+Roxas35@users.noreply.github.com>
2021-02-02 15:52:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+"uses strict";
+
+//window.location.href = "http://stackoverflow.com";
+
+/*const race=prompt("Voulez_vous adopter un chat ou un chien ?");
+if(race === "chat"){
+	window.location.href = "https://saintmalo-spa.fr/nos-chats/";
+	}
+if(race==="chien"){
+	window.location.href = "https://saintmalo-spa.fr/nos-chiens/";
+	
+	
+	
+}*/
